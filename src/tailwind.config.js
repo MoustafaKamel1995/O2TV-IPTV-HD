fa9abc2d4f8e88fc8aa0cdb5151a630f98b817ab
Moustafa Kamel <47836178+MoustafaKamel1995@users.noreply.github.com>
2024-12-31 23:43:46 +0200

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "lib/webserver/views/**/*.ejs",
+        "lib/webserver/public/js/**/*.js"
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
